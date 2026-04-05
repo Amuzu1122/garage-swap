37b2efd7233f7059b991bd0959032ec47f925581
Ernest Oduro <junioroduro676@gmail.com>
2026-04-05 20:40:35 +0000

    scrolling effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet,useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/footer";
 
 export default function App() {
+  const {pathname} = useLocation()
+
+  const hidefooter = pathname.startsWith("/market")
   return (
     <>
       <div>
         <Navbar />
         <Outlet />
-        <Footer/>
+        {!hidefooter && <Footer/>}
       </div>
     </>
   );
